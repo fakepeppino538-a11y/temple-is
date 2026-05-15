a71dbcec1ba9d574f7b75d6c4c6ccfb000447607
fakepeppino538-a11y <fakepeppino538@gmail.com>
2026-05-15 21:26:58 +0700

    Update module.js

--- module.js
+++ module.js
@@ -1,24 +1,16 @@
 var Module = {
-    arguments: [
-        "-M", "pc",
-
-        "-m", "512M",
-
-        "-cdrom", "templeos.iso",
-
-        "-boot", "d",
-
-        "-vga", "std"
-    ],
-
-    canvas: (() => {
-        const canvas = document.createElement("canvas");
-
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-        document.body.appendChild(canvas);
-
-        return canvas;
-    })()
+  arguments: [
+    "-M", "pc",
+    "-m", "512M",
+    "-cdrom", "templeos.iso",
+    "-boot", "d",
+    "-vga", "std"
+  ],
+  canvas: (() => {
+    const canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    document.body.appendChild(canvas);
+    return canvas;
+  })()
 };
